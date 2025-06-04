121169f2e572d7fe32377ef8fe20b6e1c8611bde
Ndiran <72862621+Ndiran@users.noreply.github.com>
2025-06-04 10:44:16 +0300

    Niniet third commit

--- ticketapp/urls.py
+++ ticketapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',)
+    path('tickets/', ticket_list, name='ticket_list'),
+path('tickets/submit/', submit_ticket, name='submit_ticket'),
+path('tickets/<int:ticket_id>/', ticket_detail, name='ticket_detail'),
+path('tickets/<int:ticket_id>/update/', update_ticket_status, name='update_ticket'),
 ]
